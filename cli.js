ba4d1afc1962b507b0a1d1d128a494d0ee94800a
Micha Alt <mail@lean-stack.de>
2021-05-17 21:49:43 +0200

    Install and start fastify static server.

--- cli.js
+++ cli.js
@@ -1,3 +1,26 @@
 #!/usr/bin/env node
 
+const path = require('path');
+
 console.log('Starting servir ...');
+
+// Require the framework and instantiate it
+const fastify = require('fastify')({ 
+  logger: { level: 'warn' }
+});
+
+// Register static file server
+fastify.register(require('fastify-static'), {
+  root: __dirname,
+});
+  
+// Run the server!
+const start = async () => {
+  try {
+    await fastify.listen(3000);
+  } catch (err) {
+    fastify.log.error(err);
+    process.exit(1);
+  }
+}
+start();
